14fb8e66820f9c078484c2b8449c1595267b91ed
Arthur <nashcassano8@gmail.com>
2025-09-01 16:21:04 -0300

    Finalizando o repositorytest

--- Formula1Test/Repository/PilotoRepositoryTest.cs
+++ Formula1Test/Repository/PilotoRepositoryTest.cs
@@ -1,4 +1,5 @@
-﻿using Formula1Test.Data;
+﻿using Formula1API_V2.Model;
+using Formula1Test.Data;
 using Formula1Test.Model;
 using Microsoft.EntityFrameworkCore;
 
@@ -45,4 +46,68 @@ public class PilotoRepositoryTest : IClassFixture<DatabaseFixture>, IClassFixtur
         
     }
     
+    [Fact]
+    public async Task DeveRetornarPilotoExistente()
+    {
+        var context = _context.CreateContext();
+        var piloto = _pilotoTest.Criarpiloto();
+        
+        context.Piloto.Add(piloto);
+        await context.SaveChangesAsync();
+        var pilotoExiste = await context.Piloto.AnyAsync(p => p.Id == piloto.Id);
+        Assert.True(pilotoExiste);
+    }
+
+    [Fact]
+    public async Task DeveAdicionarPiloto()
+    {
+        var context = _context.CreateContext();
+        var piloto = _pilotoTest.Criarpiloto();
+        
+        context.Piloto.Add(piloto);
+        await context.SaveChangesAsync();
+        
+        Assert.Equal(1, context.Piloto.Count());
+    }
+    
+    [Fact]
+    public async Task DeveAtualizarPiloto()
+    {
+        var context = _context.CreateContext();
+        var piloto = _pilotoTest.Criarpiloto();
+        
+        context.Piloto.Add(piloto);
+        await context.SaveChangesAsync();
+
+        var updateContext = _context.CreateContext();
+        var pilotoAtualizado = new Piloto
+        {
+            Id = piloto.Id,
+            Nome = piloto.Nome,
+            Nacionalidade = piloto.Nacionalidade,
+            DataNascimento = piloto.DataNascimento,
+            Vitorias = 10,
+            Titulos = 1,
+            Construtor = null
+        };
+        updateContext.Entry(pilotoAtualizado).State = EntityState.Modified;
+        await context.SaveChangesAsync();
+        
+        Assert.Equal(1, context.Piloto.Count());
+        
+    }
+
+    [Fact]
+    public async Task DeveDeletarUmPiloto()
+    {
+        var context = _context.CreateContext();
+        var piloto = _pilotoTest.Criarpiloto();
+        
+        context.Piloto.Add(piloto);
+        await context.SaveChangesAsync();
+        context.Remove(piloto);
+        await context.SaveChangesAsync();
+        
+        Assert.Equal(0, context.Piloto.Count());
+    }
 }
